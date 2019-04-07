2115c388c5e808bd1fde3658285905ae3cd52c9e
indrakrisna <49331418+indrakrisna@users.noreply.github.com>
2019-04-07 23:21:02 +0700

    Add files via upload

---
+++ indrakrisnalat1_akb14k_10116905_indra/app/src/main/java/com/example/zafran/indrakrisnalat1_akb14k_10116905_indra/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.zafran.indrakrisnalat1_akb14k_10116905_indra;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ indrakrisnalat1_akb14k_10116905_indra/app/src/test/java/com/example/zafran/indrakrisnalat1_akb14k_10116905_indra/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.zafran.indrakrisnalat1_akb14k_10116905_indra;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
